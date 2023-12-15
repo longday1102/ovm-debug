e20d4539427de16ccf913d7a8bde8a56d0d5b38e
Hoàng Long <longhoangai1102@gmail.com>
2023-12-15 23:47:08 +0700

    Update train_verifier.py

--- train_verifier.py
+++ train_verifier.py
@@ -39,7 +39,7 @@ if __name__ == "__main__":
         mapping = True,
     )
 
-    dataset = VDatasset_cls.dataset.set_format("torch")
+    dataset = VDataset_cls.dataset.set_format("torch")
     train_dataloader = DataLoader(
         dataset,
         batch_size = 16,
